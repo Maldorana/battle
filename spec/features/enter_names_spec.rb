ed6e65785ed2c964f91ff593d8c2785366ae9ce6
Sophie Bonneval <maldorana@gmail.com>
2021-09-07 18:02:49 +0100

    Entering players part 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,7 +3,10 @@ feature 'Enter names' do
     visit('/')
     fill_in :player_1_name, with: 'Mabon'
     fill_in :player_2_name, with: 'Sophie'
-    click_buttton 'Submit'
-    expect(page).to have_content 'Mabon vs Sophie'
+    click_button 'Submit'
+
+    save_and_open_page
+
+    expect(page).to have_content 'Mabon vs. Sophie'
   end
 end
