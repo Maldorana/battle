1745df1c39a50008e29a87b479b9cdb4ba3752f2
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-08 16:39:47 +0100

    Attacking Player 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,14 +1,18 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
-    save_and_open_page
 
     expect(page).to have_content 'Mabon vs. Sophie'
   end
   scenario 'show player 2 hit points' do
     sign_in_and_play
-    save_and_open_page
     expect(page).to have_content 'Mabon vs. Sophie'
     expect(page).to have_content "Sophie: 60HP"
+  end
+  scenario 'player 1 attacks player 2 with confirmation' do
+    sign_in_and_play
+    save_and_open_page
+    click_button 'Attack'
+    expect(page).to have_content "Mabon attacked Sophie"
   end
 end
